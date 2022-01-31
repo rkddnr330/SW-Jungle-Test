637d52755f29847724f89cb78e7be392d7a4e907
rkddnr330 <rkddnr330@nate.com>
2022-01-31 17:19:58 +0900

    API Create

--- ALONEMEMO/app.py
+++ ALONEMEMO/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+# 웹 스크래핑할 때 쓰는 beautifulSoap
 import requests
 from bs4 import BeautifulSoup
 
+# data를 부르기 위한 서버 (pymongo)
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbjungle
@@ -28,3 +30,46 @@ def saving():
 		# 2. meta tag를 스크래핑하기
 		# 3. mongoDB에 데이터 넣기
     return jsonify({'result': 'success', 'msg':'POST 연결되었습니다!'})
+
+
+# 메모를 작성하기 위해 서버가 전달받아야하는 정보는 다음 두 가지입니다.
+# - URL(url_give)
+# - 코멘트(comment_give)
+
+# 그리고 URL를 meta tag를 스크래핑해서 아래 데이터를 저장(Create)합니다.
+# - URL(url)
+# - 제목(title)
+# - 설명(desc)
+# - 이미지URL(image)
+# - 코멘트(comment)
+
+# 따라서 서버 로직은 다음 단계로 구성되어야 합니다.
+# 1. 클라이언트로부터 데이터를 받기
+# 2. meta tag를 스크래핑하기
+# 3. mongoDB에 데이터를 넣기
+
+@app.route('/memo', methods=['POST'])
+def post_articles():
+    # 1. 클라이언트로부터 데이터 받기 (입력한 url, 입력한 comment)
+    url_receive = response.form['url_give']
+    comment_receive = response.form['comment_give']
+
+    # 2. meta tag를 스크래핑하기
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    url_title = og_title['content']
+    url_description = og_description['content']
+    url_image = og_image['content']
+
+    article = {'url':url_receive, 'title':url_title, 'description':url_description, 'image':url_image, 'comment':comment_receive}
+
+    # 3. mongoDB에 데이터 넣기
+    db.articles.insert_one(article)
+
+    return jsonify({'result':'success'})
