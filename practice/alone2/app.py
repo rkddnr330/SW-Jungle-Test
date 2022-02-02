f4de0f620e1f83d45c283119ae3c8555c334ef71
rkddnr330 <rkddnr330@nate.com>
2022-02-02 17:11:39 +0900

    [alone2] 리스팅API & finish

--- practice/alone2/app.py
+++ practice/alone2/app.py
@@ -6,21 +6,11 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 client = MongoClient('localhost',27017)
-db = client.dbjungle
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/memo',methods=['POST'])
-def listing():
-    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    # 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!'})
-
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
-
 
 @app.route('/memo',methods=['POST'])
 def post_articles():
@@ -34,17 +24,28 @@ def post_articles():
 
     soup = BeautifulSoup(data.text, 'html.parser')  
 
-    og_image = soup.select_one('meta[property="og_image"]')
-    og_title = soup.select_one('meta[propery="og_title"]')
-    og_description = soup.select_one('meta[propery="og_description"]')
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
 
     url_image = og_image['content']
     url_title = og_title['content']
     url_description = og_description['content']
 
-    article = {'url':url_receive, 'title':url_title,'image':url_image, 'desc':url_description, 'comment':comment_receive}
+    article = {'url':url_receive, 'title':url_title,'desc':url_description,'image':url_image, 'comment':comment_receive}
     
     # 3. mongoDB에 데이터를 넣기
     db.articles.insert_one(article)
 
     return jsonify({'result':'success'})
+
+@app.route('/memo',methods=['GET'])
+def read_articles():
+    # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
+    result = list(db.articles.find({},{'_id':0}))
+
+    # 2. articles라는 키 값으로 영화정보 내려주기
+    return jsonify({'result':'success', 'articles':result})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
